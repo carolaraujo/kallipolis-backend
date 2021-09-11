56bd9a6354410bd7d44929452251ea180adb2334
Caroline Araujo <ca.araujosouza@gmail.com>
2021-09-11 18:44:31 -0300

    first commit

---
+++ src/main/java/com/br/kallipolis/KallipolisApplication.java
@@ -0,0 +1,13 @@
+package com.br.kallipolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KallipolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KallipolisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/kallipolis/KallipolisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.kallipolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KallipolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
